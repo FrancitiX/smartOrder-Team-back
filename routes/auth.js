5884eda63e8b53f5bfe6176b3007ff9c80f5b505
FrancitiX <gamercrepy8@gmail.com>
2025-11-23 22:39:37 -0600

    fix: Middleware

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ const orderController = require("../src/Api/Order/orderController");
 const placeController = require("../src/Api/Places/placeController");
 const configurationsController = require("../src/Api/Configs/configController");
 
-const authMiddleware = require("../src/middleware/authMiddleware");
+const authMiddleware = require("../src/Middleware/authMiddleware");
 
 // Rutas para autenticación de usuarios
 router.post("/newUser", authMiddleware, userController.registerUser);
